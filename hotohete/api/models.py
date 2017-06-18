80006ae45b2e14d84cfa1c6216cd7cff3fe1572e
Rubén Pardo <yosoyruben@gmail.com>
2017-06-18 13:09:53 +0200

    Created web application and first views rendering teams and users

--- hotohete/api/models.py
+++ hotohete/api/models.py
@@ -7,7 +7,13 @@ from django.db import models
 class CSTeam(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class CSUser(models.Model):
     steam_username = models.CharField(max_length=255)
     team = models.ForeignKey(CSTeam, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.steam_username
