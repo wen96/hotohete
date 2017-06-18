31d1d753d9ecc2461aac068b78af2a0acd85ba69
Rubén Pardo <yosoyruben@gmail.com>
2017-06-18 14:23:42 +0200

    Allowed hosts

--- hotohete/production.py
+++ hotohete/production.py
@@ -4,3 +4,7 @@ from hotohete.settings import *
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = [
+    'hotohete.herokuapp.com'
+]
