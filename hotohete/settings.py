63a10deb8e7e91bb7726f64467b9e956f30ce4d1
Rubén Pardo <yosoyruben@gmail.com>
2017-06-25 10:24:59 +0200

    One more try! tarara tarara

--- hotohete/settings.py
+++ hotohete/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,6 +118,8 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
