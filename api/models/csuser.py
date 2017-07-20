5c1e2fd6fe2dcbe93783687981e8a99cd062db25
Rubén Pardo <yosoyruben@gmail.com>
2017-07-20 20:14:19 +0200

    Created function calculating total kills per user and showing on html percentage over total_kills

--- api/models/csuser.py
+++ api/models/csuser.py
@@ -6,6 +6,7 @@ class CSUser(models.Model):
     steam_username = models.CharField(max_length=255)
     team = models.ForeignKey('api.CSTeam', on_delete=models.SET_NULL, null=True)
     steam_id = models.CharField(max_length=126, null=True, blank=True)
+    csgo_info = None
 
     steam_service = SteamAPIService()
 
@@ -25,8 +26,50 @@ class CSUser(models.Model):
 
     @property
     def get_cs_info(self):
-        return self.steam_service.get_cs_info(self.get_steam_id)
+        if not self.csgo_info:
+            self.csgo_info = {stat['name']: stat['value'] for stat in self.steam_service.get_cs_info(self.get_steam_id)}
+        return self.csgo_info
 
     @property
-    def get_weapon_group_per(self):
-        return self.steam_service.get_weapon_group_per(self.get_steam_id)
+    def category_weapons_kills(self):
+        """ {'total_kills_p90': 123, 'total_kills_awp': 33}
+
+            {
+                'p90': 123,
+                'awp': 123
+            }
+        """
+        weapons_kills = {}
+
+        smgs = ['p90', 'bizon', 'ump45', 'mp7', 'mp9']
+        snipers = ['awp', 'ssg08']
+        pistols = ['glock', 'deagle', 'elite', 'fiveseven', 'hkp2000', 'p250']
+        rifles = ['ak47', 'm4a1', 'famas', 'galilar']
+        shotguns = ['mag7', 'xm1014', 'nova', 'sawedoff']
+
+        weapons_kills = {
+            'smg': 0,
+            'sniper': 0,
+            'pistol': 0,
+            'rifle': 0,
+            'shotgun': 0,
+        }
+
+        for smg in smgs:
+            weapons_kills['smg'] += self.csgo_info['total_kills_{}'.format(smg)]
+
+        for smg in snipers:
+            weapons_kills['sniper'] += self.csgo_info['total_kills_{}'.format(smg)]
+
+        for smg in pistols:
+            weapons_kills['pistol'] += self.csgo_info['total_kills_{}'.format(smg)]
+
+        for smg in rifles:
+            weapons_kills['rifle'] += self.csgo_info['total_kills_{}'.format(smg)]
+
+        for smg in shotguns:
+            weapons_kills['shotgun'] += self.csgo_info['total_kills_{}'.format(smg)]
+
+        weapons_kills['other'] = self.csgo_info['total_kills'] - sum(weapons_kills.values())
+
+        return weapons_kills
