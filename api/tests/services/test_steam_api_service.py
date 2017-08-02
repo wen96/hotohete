960dabf621a9a9d9ac3eecdde45399a132241ca3
Javier Moreno <javier.moreno.vilaplana@hotmail.com>
2017-08-02 21:33:43 +0200

    added new test to cs_info

--- api/tests/services/test_steam_api_service.py
+++ api/tests/services/test_steam_api_service.py
@@ -10,7 +10,7 @@ class SteamApiServiceTestCase(TestCase):
     @mock.patch.object(SteamAPIService, 'api_key')
     def test_get_steam_id_from_nick_name_returns_none_if_user_not_found_in_reponse(self, mock_api_key, mock_request_endpoint):
         # Arrange
-        mock_api_key.return_value = 'morcilla'
+        mock_api_key.return_value = 'hummus'
         mock_request_endpoint.return_value = {'response': {'error': 'asdf'}}
         nickname = 'clapton'
 
@@ -25,7 +25,7 @@ class SteamApiServiceTestCase(TestCase):
     @mock.patch.object(SteamAPIService, 'api_key')
     def test_get_steam_id_from_nick_name_returns_steam_id(self, mock_api_key, mock_request_endpoint):
         # Arrange
-        mock_api_key.return_value = 'morcilla'
+        mock_api_key.return_value = 'hummus'
         mock_request_endpoint.return_value = {'response': {'steamid': '12341234'}}
         nickname = 'clapton'
 
@@ -36,17 +36,49 @@ class SteamApiServiceTestCase(TestCase):
         # Asssert
         self.assertEqual(result, '12341234')
 
-    def test_init_api_key_is_none(self):
+    def test__init__default_var(self):
+        #  Act
         service = SteamAPIService()
 
-        self.assertIsNone(service.api_key)
-
-    def test_init_cache_on_creation_is_empty(self):
-        service = SteamAPIService()
-
+        #  Assert
+        self.assertIsNone(service._api_key)
         self.assertEqual(service.cache_steam_info, {})
-
-    def test_init_base_url_(self):
-        service = SteamAPIService()
-
         self.assertEqual(service.base_url, 'http://api.steampowered.com')
+
+    def test_cs_info_returns_none_if_steam_id_is_none(self):
+        #  Act
+        steam_id = None
+        service = SteamAPIService()
+        result = service.get_cs_info(steam_id)
+
+        #  Assert
+        self.assertIsNone(result)
+
+    @mock.patch.object(SteamAPIService, '_request_endpoint')
+    @mock.patch.object(SteamAPIService, 'api_key')
+    def test_cs_info_returns_none_cause_playerstats_not_found(self, mock_api_key, mock_request_endpoint):
+        # Arrange
+        mock_api_key.return_value = 'hummus'
+        mock_request_endpoint.return_value = False
+        steam_id = 1234
+
+        #  Act
+        service = SteamAPIService()
+        result = service.get_cs_info(steam_id)
+
+        # Assert
+        self.assertIsNone(result)
+
+    """
+    @mock.patch.object(SteamAPIService, '_request_endpoint')
+    @mock.patch.object(SteamAPIService, 'api_key')
+    def test_cs_info_returns_playerstats(self, mock_api_key, mock_request_endpoint):
+        mock_api_key.return_value = 'hummus'
+        mock_request_endpoint.return_value = {'playerstats': {'stats'}}
+        steam_id = 1234
+
+        #  Act
+        service = SteamAPIService()
+        result = service.get_cs_info(steam_id)
+        self.assertEqual(result, {'playerstats': {'stats'}})
+    """
