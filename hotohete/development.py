b0edae63ee6e992186feee0449fecc11afce56bf
Rubén Pardo <yosoyruben@gmail.com>
2017-09-16 13:42:49 +0200

    Setting up dev environment to work with postgresql not sqlite

---
+++ hotohete/development.py
@@ -0,0 +1,10 @@
+from hotohete.settings import *
+
+
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.postgresql',
+    'HOST': 'localhost',
+    'NAME': 'hotohete',
+    'USER': 'hotohete',
+    'PASSWORD': 'testtest',
+}
