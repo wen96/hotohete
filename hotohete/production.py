92e447fe1e50b73f13c98f7ac24ac17b9d0bda86
Rubén Pardo <yosoyruben@gmail.com>
2017-09-16 10:48:02 +0200

    Change production settings to a secure set up

--- hotohete/production.py
+++ hotohete/production.py
@@ -1,15 +1,21 @@
 import dj_database_url
 from hotohete.settings import *
 
+DEBUG = False
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = [
     'hotohete.herokuapp.com',
-    'hotohetestaging.herokuapp.com',
-    'localhost',
-    '127.0.0.1'
+    'hotohetestaging.herokuapp.com'
 ]
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
